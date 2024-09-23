5b33f267569e2d073f9b4acf0828462ef0b790e8
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-23 18:06:22 +0900

    Implement EFI loader

--- HinatsukiLoaderPkg/Main.c
+++ HinatsukiLoaderPkg/Main.c
@@ -1,5 +1,7 @@
+#include "elf.hpp"
 #include "frame_buffer_config.hpp"
 #include <Guid/FileInfo.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -175,6 +177,35 @@ void Halt(void) {
   }
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr *ehdr, UINT64 *first, UINT64 *last) {
+  Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64)ehdr + ehdr->e_phoff);
+  *first = MAX_UINT64;
+  *last = 0;
+
+  for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+    if (phdr[i].p_type != PT_LOAD) {
+      continue;
+    }
+
+    *first = MIN(*first, phdr[i].p_vaddr);
+    *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+  }
+}
+
+void CopyLoadSegments(Elf64_Ehdr *ehdr) {
+  Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64)ehdr + ehdr->e_phoff);
+  for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+    if (phdr[i].p_type != PT_LOAD) {
+      continue;
+    }
+
+    UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+    CopyMem((VOID *)phdr[i].p_vaddr, (VOID *)segm_in_file, phdr[i].p_filesz);
+    SetMem((VOID *)(phdr[i].p_vaddr + phdr[i].p_filesz),
+           phdr[i].p_memsz - phdr[i].p_filesz, 0);
+  }
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
   EFI_STATUS status;
@@ -267,23 +298,40 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
   EFI_FILE_INFO *file_info = (EFI_FILE_INFO *)file_info_buffer;
   UINTN kernel_file_size = file_info->FileSize;
 
-  EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-  status = gBS->AllocatePages(AllocateAddress, EfiLoaderData,
-                              (kernel_file_size + 0xfff) / 0x1000,
-                              &kernel_base_addr);
+  VOID *kernel_buffer;
+  status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
   if (EFI_ERROR(status)) {
-    Print(L"Failed to allocate pages: %r\n", status);
+    Print(L"Failed to allocate pool: %r\n", status);
     Halt();
   }
 
-  status = kernel_file->Read(kernel_file, &kernel_file_size,
-                             (VOID *)kernel_base_addr);
+  status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
   if (EFI_ERROR(status)) {
     Print(L"Failed to read file: %r\n", status);
     Halt();
   }
 
-  Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+  Elf64_Ehdr *kernel_ehdr = (Elf64_Ehdr *)kernel_buffer;
+  UINT64 kernel_first_addr, kernel_last_addr;
+  CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+  UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+  status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages,
+                              &kernel_first_addr);
+  if (EFI_ERROR(status)) {
+    Print(L"Failed to allocate pages: %r\n", status);
+    Halt();
+  }
+
+  CopyLoadSegments(kernel_ehdr);
+
+  Print(L"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+  status = gBS->FreePool(kernel_buffer);
+  if (EFI_ERROR(status)) {
+    Print(L"Failed to free pool: %r\n", status);
+    Halt();
+  }
 
   status = gBS->ExitBootServices(image_handle, memmap.map_key);
   if (EFI_ERROR(status)) {
@@ -300,7 +348,7 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
     }
   }
 
-  UINT64 entry_addr = *(UINT64 *)(kernel_base_addr + 24);
+  UINT64 entry_addr = *(UINT64 *)(kernel_first_addr + 24);
 
   struct FrameBufferConfig config = {(UINT8 *)gop->Mode->FrameBufferBase,
                                      gop->Mode->Info->PixelsPerScanLine,
--- Makefile
+++ Makefile
@@ -13,11 +13,11 @@ dest/disk.img: deps/edk2/Build/HinatsukiLoaderX64/DEBUG_CLANGPDB/X64/Loader.efi 
 deps/edk2/BaseTools/Source/C/bin/GenFw:
 	make -C deps/edk2/BaseTools/Source/C
 
-deps/edk2/Build/HinatsukiLoaderX64/DEBUG_CLANGPDB/X64/Loader.efi: deps/edk2/BaseTools/Source/C/bin/GenFw HinatsukiLoaderPkg/Main.c HinatsukiLoaderPkg/Loader.inf HinatsukiLoaderPkg/HinatsukiLoaderPkg.dec HinatsukiLoaderPkg/HinatsukiLoaderPkg.dsc kernel/frame_buffer_config.hpp
+deps/edk2/Build/HinatsukiLoaderX64/DEBUG_CLANGPDB/X64/Loader.efi: deps/edk2/BaseTools/Source/C/bin/GenFw HinatsukiLoaderPkg/Main.c HinatsukiLoaderPkg/Loader.inf HinatsukiLoaderPkg/HinatsukiLoaderPkg.dec HinatsukiLoaderPkg/HinatsukiLoaderPkg.dsc kernel/elf.hpp kernel/frame_buffer_config.hpp
 	bash -c "cd deps/edk2 && source edksetup.sh && build"
 
 dest/kernel.elf: kernel/main.o
-	ld.lld -L./deps/x86_64-elf/lib --entry KernelMain -z norelro -z separate-loadable-segments --image-base 0x100000 --static -o $@ $<
+	ld.lld -L./deps/x86_64-elf/lib --entry KernelMain -z norelro --image-base 0x100000 --static -o $@ $<
 
 kernel/main.o: kernel/main.cpp kernel/frame_buffer_config.hpp
 	clang++ -I./deps/x86_64-elf/include -I./deps/x86_64-elf/include/c++/v1 -nostdlibinc -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++20 -c $< -o $@

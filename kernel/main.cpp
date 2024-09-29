34954cdc7cdaa53e14689dde0012568f2d520eed
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-29 17:15:06 +0900

    Implement `printk`

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,11 +1,29 @@
 #include "console.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
+#include <cstdarg>
 #include <cstdint>
 #include <cstdio>
 #include <new>
 
 void operator delete(void *_) noexcept {}
+
+char console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsnprintf(s, sizeof(s), format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
@@ -29,12 +47,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {255, 255, 255}, {0, 0, 0}};
+  console =
+      new (console_buf) Console{*pixel_writer, {255, 255, 255}, {0, 0, 0}};
 
-  char buf[128];
   for (int i = 0; i < 26; i++) {
-    snprintf(buf, 128, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (true) {
